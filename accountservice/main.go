fa0bcea4df76d47930ae2c204794fd740b24b37e
Tran Ngoc Dan <dantn93@gmail.com>
2019-07-03 10:06:48 +0700

    finish P8

--- accountservice/main.go
+++ accountservice/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-        "fmt"
-        "github.com/goblog/accountservice/service"
-        "github.com/goblog/accountservice/dbclient"
+	"flag"
+	"fmt"
+
+	"github.com/goblog/accountservice/config"
+	"github.com/goblog/accountservice/dbclient"
+	"github.com/goblog/accountservice/service"
+	"github.com/spf13/viper"
 )
 
 var appName = "accountservice"
 
+// Init function, runs before main()
+func init() {
+	// Read command line flags
+	profile := flag.String("profile", "test", "Environment profile, something similar to spring profiles")
+	configServerUrl := flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
+	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
+	flag.Parse()
+	// Pass the flag values into viper.
+	viper.Set("profile", *profile)
+	viper.Set("configServerUrl", *configServerUrl)
+	viper.Set("configBranch", *configBranch)
+}
+
 func main() {
-        fmt.Printf("Starting %v\n", appName)
-        initializeBoltClient()
-        service.StartWebServer("6767")
+	fmt.Printf("Starting %v\n", appName)
+	// NEW - load the config
+	config.LoadConfigurationFromBranch(
+		viper.GetString("configServerUrl"),
+		appName,
+		viper.GetString("profile"),
+		viper.GetString("configBranch"))
+	initializeBoltClient()
+	go config.StartListener(appName, viper.GetString("amqp_server_url"), viper.GetString("config_event_bus"))
+	service.StartWebServer(viper.GetString("server_port")) // NEW, use port from loaded config
 }
 
 func initializeBoltClient() {
-        service.DBClient = &dbclient.BoltClient{}
-        service.DBClient.OpenBoltDb()
-        service.DBClient.Seed()
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
 }
---
+++ support/config-server/scripts.go
@@ -0,0 +1,1 @@
+// keytool -genkeypair -alias goblogkey -keyalg RSA -dname "CN=Go Blog,OU=Unit,O=Organization,L=City,S=State,C=SE" -keypass changeme -keystore server.jks -storepass letmein -validity 730
